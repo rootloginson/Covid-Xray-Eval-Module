ab791c430bfafd285f68ef37e0776afdf85c492b
pc_git <64226516+rootloginson@users.noreply.github.com>
2021-02-25 20:58:54 +0300

    imageTransform docstring edit

--- imageTransform.py
+++ imageTransform.py
@@ -4,13 +4,9 @@ from PIL import Image
 
 
 def apply_transforms(img):
-    """
-    Import These Libraries:
-        "import torchvision.transforms as transforms"
-        "from PIL import convert"
+    """This function call is located in "makeInputforCNN" function."""
 
-    !! This function call is located in "makeInputforCNN" function.
-    """
+    # Normalization parameters for transforms.Normalize() transformation.
     mu = 0.570406436920166
     sigma = 0.1779220998287201
 
@@ -48,9 +44,8 @@ def makeInputforCNN(img_path):
     transformed_img = transformed_img.unsqueeze(0)
     # check dimension
     assert transformed_img.shape == torch.Size([1, 3, 224, 224]), "model input image size is not valid"
-
     return transformed_img
 
-# debug reasons
+
 if __name__ == '__main__':
     print("Image Transform module is running as main module for no reason.(Probably @.o)")
